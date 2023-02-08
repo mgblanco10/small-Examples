840d1dc847b021b723407bc17813633e22d033d5
mgblanco10 <mgblanco1001@gmail.com>
2023-02-08 16:52:01 +0100

    exercise improvements

--- DOM/form/index.js
+++ DOM/form/index.js
@@ -12,7 +12,32 @@ window.addEventListener('load', function(){
 
         let name = document.querySelector("#name").value;
         let lastName = document.querySelector("#lastName").value;
-        let age = document.querySelector("#age").value;
+        let age = parseInt(document.querySelector("#age").value);
+
+        if(name.trim() == null || name.trim().length == 0){
+            // alert("The name is invalid");
+            document.querySelector("#notValidName").innerHTML = "The name you have entered is not correct";
+            return false;
+        }else{
+            document.querySelector("#notValidName").style.display = "none";
+        }
+
+        if(lastName.trim() == null || lastName.trim().length == 0){
+            // alert("The last name is invalid");
+            document.querySelector("#notValidLastName").innerHTML = "The last name you have entered is not correct";
+            return false;
+        }else{
+            document.querySelector("#notValidLastName").style.display = "none";
+        }
+
+        if(age == null || age.length <= 0 || isNaN(age)){
+            // alert("The age is invalid");
+            document.querySelector("#notValidAge").innerHTML = "The age you have entered is not correct";
+            return false;
+        }else{
+            document.querySelector("#notValidAge").style.display = "none";
+        }
+        
 
         boxDashed.style.display = "block";
 
