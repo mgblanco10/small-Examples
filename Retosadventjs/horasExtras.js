56fc83e22846d21260ba7543047befe00fd6fb74
mgblanco10 <mgblanco1001@gmail.com>
2023-03-28 09:30:45 +0200

    countHour extra not includes satuday and sunday

--- Retosadventjs/horasExtras.js
+++ Retosadventjs/horasExtras.js
@@ -1,41 +1,54 @@
-function countHours(year, holidays) {
-    const numDaysInMonth = [31, (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0 ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    let extraHours = 0;
-  
-    const isHoliday = (date) => {
-      for (let i = 0; i < holidays.length; i++) {
-        const [month, day] = holidays[i].split('/');
-        const holidayDate = new Date(year, month - 1, day);
-        if (date.getTime() === holidayDate.getTime()) {
-          return true;
+function countHours(year, holidays){
+    let extraHours = 0
+    for(let holiday of holidays){
+        const date = new Date (`${holiday}/${year}`)
+        
+            
+            // 0 domingo y 6 sábado
+            if (![0, 6].includes(date.getDay())){
+                extraHours += 2
+            }
         }
-      }
-      return false;
-    };
-  
-    for (let month = 0; month < 12; month++) {
-      for (let day = 1; day <= numDaysInMonth[month]; day++) {
-        const date = new Date(year, month, day);
-        if (date.getDay() !== 0 && date.getDay() !== 6 && !isHoliday(date)) {
-          extraHours++;
-        }
-      }
-    }
-  
-    for (let i = 0; i < holidays.length; i++) {
-      const [month, day] = holidays[i].split('/');
-      const holidayDate = new Date(year, month - 1, day);
-      if (holidayDate.getDay() !== 0 && holidayDate.getDay() !== 6 && !isHoliday(holidayDate)) {
-        extraHours += 2;
-      }
-    }
-  
-    return extraHours;
-  }
-
+    
+    return extraHours
+}
 
   console.log(countHours(2023, ['01/06', '04/01', '12/25'])); // expected output: 4
-  console.log(countHours(2021, ['01/01', '05/01', '07/28', '12/25'])); // expected output: 0
+  console.log(countHours(2021, ['01/01', '05/01', '07/28', '12/25'])); // expected output: 4
   console.log(countHours(2022, ['01/06', '04/01', '12/25'])); // expected output: 4
-  console.log(countHours(2023, ['01/01', '05/01', '07/28', '12/25'])); // expected output: 8
+  console.log(countHours(2023, ['01/01', '05/01', '07/28', '12/25'])); // expected output: 6
   
+  // function countHours(year, holidays) {
+  //     const numDaysInMonth = [31, (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0 ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  //     let extraHours = 0;
+    
+  //     const isHoliday = (date) => {
+  //       for (let i = 0; i < holidays.length; i++) {
+  //         const [month, day] = holidays[i].split('/');
+  //         const holidayDate = new Date(year, month - 1, day);
+  //         if (date.getTime() === holidayDate.getTime()) {
+  //           return true;
+  //         }
+  //       }
+  //       return false;
+  //     };
+    
+  //     for (let month = 0; month < 12; month++) {
+  //       for (let day = 1; day <= numDaysInMonth[month]; day++) {
+  //         const date = new Date(year, month, day);
+  //         if (date.getDay() !== 0 && date.getDay() !== 6 && !isHoliday(date)) {
+  //           extraHours++;
+  //         }
+  //       }
+  //     }
+    
+  //     for (let i = 0; i < holidays.length; i++) {
+  //       const [month, day] = holidays[i].split('/');
+  //       const holidayDate = new Date(year, month - 1, day);
+  //       if (holidayDate.getDay() !== 0 && holidayDate.getDay() !== 6 && !isHoliday(holidayDate)) {
+  //         extraHours += 2;
+  //       }
+  //     }
+    
+  //     return extraHours;
+  //   }
