a8cf365f66e11538d249db7330454d75d9132ad9
mgblanco10 <mgblanco1001@gmail.com>
2023-03-28 09:34:45 +0200

    mejoras e incluir el enunciado del reto en el encabezado

--- Retosadventjs/horasExtras.js
+++ Retosadventjs/horasExtras.js
@@ -1,3 +1,9 @@
+// Un millonario ha comprado una red social y no trae buenas noticias. Ha anunciado que cada vez que una jornada de trabajo se pierde por un día festivo, habrá que compensarlo con dos horas extra otro día de ese mismo año.
+// Obviamente la gente que trabaja en la empresa no le ha hecho ni pizca de gracia y están preparando un programa que les diga el número de horas extras que harían en el año si se aplicara la nueva norma.
+// Al ser trabajo de oficina, su horario laboral es de lunes a viernes. Así que sólo tienes que preocuparte de los días festivos que caen en esos días.
+// El año puede ser bisiesto. Haz las comprobaciones que necesitas para ello, si fuese necesario.
+// Aunque el 31 de diciembre sea festivo, las horas extra se harán el mismo año y no el siguiente.
+// El método Date.getDay() te devuelve el día de la semana de una fecha. El 0 es domingo, el 1 es lunes, etc.
 function countHours(year, holidays){
     let extraHours = 0
     for(let holiday of holidays){
@@ -18,37 +24,3 @@ function countHours(year, holidays){
   console.log(countHours(2022, ['01/06', '04/01', '12/25'])); // expected output: 4
   console.log(countHours(2023, ['01/01', '05/01', '07/28', '12/25'])); // expected output: 6
   
-  // function countHours(year, holidays) {
-  //     const numDaysInMonth = [31, (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0 ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-  //     let extraHours = 0;
-    
-  //     const isHoliday = (date) => {
-  //       for (let i = 0; i < holidays.length; i++) {
-  //         const [month, day] = holidays[i].split('/');
-  //         const holidayDate = new Date(year, month - 1, day);
-  //         if (date.getTime() === holidayDate.getTime()) {
-  //           return true;
-  //         }
-  //       }
-  //       return false;
-  //     };
-    
-  //     for (let month = 0; month < 12; month++) {
-  //       for (let day = 1; day <= numDaysInMonth[month]; day++) {
-  //         const date = new Date(year, month, day);
-  //         if (date.getDay() !== 0 && date.getDay() !== 6 && !isHoliday(date)) {
-  //           extraHours++;
-  //         }
-  //       }
-  //     }
-    
-  //     for (let i = 0; i < holidays.length; i++) {
-  //       const [month, day] = holidays[i].split('/');
-  //       const holidayDate = new Date(year, month - 1, day);
-  //       if (holidayDate.getDay() !== 0 && holidayDate.getDay() !== 6 && !isHoliday(holidayDate)) {
-  //         extraHours += 2;
-  //       }
-  //     }
-    
-  //     return extraHours;
-  //   }
